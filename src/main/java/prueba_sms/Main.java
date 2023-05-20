278a4d9a5f4aa231936e6fdc330d26056fbb0a81
EstebanNavas <arcoesteb@live.com>
2023-05-19 21:54:10 -0500

    Se crea array numerosTelefonicos y ciclo for para iterar y enviar sms a cada número del array

--- src/main/java/prueba_sms/Main.java
+++ src/main/java/prueba_sms/Main.java
@@ -47,61 +47,69 @@ public class Main {
 		parametersList.add(new BasicNameValuePair("login", "noe.herrera@mobile-tic.com"));
 		parametersList.add(new BasicNameValuePair("passwd", "M7Tc9pXbZh3d"));
 		
-		// Descomentar para utilizar la autentificación mediante apikey
-		// parametersList.add(new BasicNameValuePair("apikey", "YY"));
-		// parametersList.add(new BasicNameValuePair("apisecret", "ZZ"));
-		parametersList.add(new BasicNameValuePair("dest", "573217048602"));
-		//
-		parametersList.add(new BasicNameValuePair("msg", "Mensaje de prueba desde la API de Altiria"));
-		// No es posible utilizar el remitente en América pero sí en España y Europa
-		// Descomentar la línea solo si se cuenta con un remitente autorizado por
-		// Altiria
-		// parametersList.add(new BasicNameValuePair("senderId", "remitente"));
+		
+		// Array de números telefonicos
+		String[] numerosTelefonicos = new String[2];
+		
+        numerosTelefonicos[0] = "573217048602";
+        numerosTelefonicos[1] = "573103593237";
+//        numerosTelefonicos[2] = "573113873338";
+//        numerosTelefonicos[3] = "573116251502";
 
-		try {
-			// Se fija la codificacion de caracteres de la peticion POST
-			post.setEntity(new UrlEncodedFormEntity(parametersList, "UTF-8"));
-		} catch (UnsupportedEncodingException uex) {
-			System.out.println("ERROR: codificación de caracteres no soportada");
-		}
+        for (int i = 0; i < numerosTelefonicos.length; i++) {
+            parametersList.add(new BasicNameValuePair("dest", numerosTelefonicos[i]));
+            parametersList.add(new BasicNameValuePair("msg", "Mensaje de prueba desde la API de Altiria"));
+            
+            try {
+    			// Se fija la codificacion de caracteres de la peticion POST
+    			
+    			post.setEntity(new UrlEncodedFormEntity(parametersList, "UTF-8"));
+    		} catch (UnsupportedEncodingException uex) {
+    			System.out.println("ERROR: codificación de caracteres no soportada");
+    		}
 
-		CloseableHttpResponse response = null;
+    		CloseableHttpResponse response = null;
 
-		try {
-			System.out.println("Enviando petición");
-			// Se envía la petición
-			response = httpClient.execute(post);
-			// Se consigue la respuesta
-			String resp = EntityUtils.toString(response.getEntity());
+    		try {
+    			System.out.println("Enviando petición");
+    			// Se envía la petición
+    			response = httpClient.execute(post);
+    			// Se consigue la respuesta
+    			String resp = EntityUtils.toString(response.getEntity());
 
-			// Error en la respuesta del servidor
-			if (response.getStatusLine().getStatusCode() != 200) {
-				System.out.println("ERROR: Código de error HTTP:  " + response.getStatusLine().getStatusCode());
-				System.out.println("Compruebe que ha configurado correctamente la direccion/url ");
-				System.out.println("suministrada por Altiria");
-				return;
-			} else {
-				// Se procesa la respuesta capturada en la cadena 'response'
-				if (resp.startsWith("ERROR")) {
-					System.out.println(resp);
-					System.out.println("Código de error de Altiria. Compruebe las especificaciones");
-				} else
-					System.out.println(resp);
-			}
-		} catch (Exception e) {
-			System.out.println("Excepción");
-			e.printStackTrace();
-			return;
-		} finally {
-			// En cualquier caso se cierra la conexión
-			post.releaseConnection();
-			if (response != null) {
-				try {
-					response.close();
-				} catch (IOException ioe) {
-					System.out.println("ERROR cerrando recursos");
-				}
-			}
-		}
+    			// Error en la respuesta del servidor
+    			if (response.getStatusLine().getStatusCode() != 200) {
+    				System.out.println("ERROR: Código de error HTTP:  " + response.getStatusLine().getStatusCode());
+    				System.out.println("Compruebe que ha configurado correctamente la direccion/url ");
+    				System.out.println("suministrada por Altiria");
+    				return;
+    			} else {
+    				// Se procesa la respuesta capturada en la cadena 'response'
+    				if (resp.startsWith("ERROR")) {
+    					System.out.println(resp);
+    					System.out.println("Código de error de Altiria. Compruebe las especificaciones");
+    				} else
+    					System.out.println(resp);
+    			}
+    		} catch (Exception e) {
+    			System.out.println("Excepción");
+    			e.printStackTrace();
+    			return;
+    		} finally {
+    			// En cualquier caso se cierra la conexión
+    			post.releaseConnection();
+    			if (response != null) {
+    				try {
+    					response.close();
+    				} catch (IOException ioe) {
+    					System.out.println("ERROR cerrando recursos");
+    				}
+    			}
+    		}
+            
+        }  //Fin del FOR
+	
+
+		
 	}
 }
