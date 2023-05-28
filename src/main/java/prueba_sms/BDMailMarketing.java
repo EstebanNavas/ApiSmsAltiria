ac9844c9fa857484889ef34a8b625ae1d0eb7580
EstebanNavas <arcoesteb@live.com>
2023-05-28 00:06:19 -0500

    Se crean los métodos de consulta: consultarLocalesPorId, consultarPeriodoFechaPago y consultarTelefonoCelular

--- src/main/java/prueba_sms/BDMailMarketing.java
+++ src/main/java/prueba_sms/BDMailMarketing.java
@@ -1,6 +1,7 @@
 package prueba_sms;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;    //pool de conexiones jdbc
@@ -15,14 +16,23 @@ public class BDMailMarketing {
         // Verificamos si la coenxion es éxitosa o no 
         if (connection != null) {
             try {
-                // Creamos una declaración con el objeto Statement
-                Statement statement = connection.createStatement();
+              
 
-                // Ejecutamos una consulta donde le pasamos el nombre de la tabla a consultar, el resultado se guarda en el objeto resultSet
-                String query = "SELECT * FROM BDMailMarketing.dbo.tblMailCampaign";                                 
-                ResultSet resultSet = statement.executeQuery(query);
+                // Ejecutamos una consulta parametrizada. . Los signos de interrogación (?) se utilizan como marcadores de posición para los parámetros.
+                String query = "SELECT * FROM BDMailMarketing.dbo.tblMailCampaign WHERE idlocal = ? AND sistema = ? AND idCampaign = ?";
+                PreparedStatement statement = connection.prepareStatement(query);
+                
+             // Asignamos los valores de los parámetros en la consulta
+                statement.setInt(1, 100); //idlocal
+                statement.setString(2, "aquamovil");  //sistema
+                statement.setInt(3, 18); //idCampaign
+                
+                		                              
+              //Se ejecuta la consulta utilizando el método executeQuery() del objeto PreparedStatement y se guarda el resultado en el objeto ResultSet.  
+                ResultSet resultSet = statement.executeQuery();
 
      
+                
                 
                 // Recorremos los resultados de la consulta usando el metodo next() para recorrer fila por fila los registros obtenidos en la consulta
                 while (resultSet.next()) {
@@ -30,10 +40,13 @@ public class BDMailMarketing {
                     //  Utilizamos los métodos getInt() y getString() del ResultSet para obtener los valores de cada columna en el registro actual
                     int idLocal = resultSet.getInt("IDLOCAL");                   
                     String NombreCampaign = resultSet.getString("NombreCampaign");
+                    String sistema = resultSet.getString("sistema");
+                    int idCampaign = resultSet.getInt("idCampaign");
+                    String textoSMS = resultSet.getString("textoSMS");
                     
 
                     // Mostramos los valores por consola
-                      System.out.println("IDlocal: " + idLocal + "  NombreCampaign " + NombreCampaign );                    
+                      System.out.println("IDlocal: " + idLocal + "  NombreCampaign " + NombreCampaign + " sistema " + sistema + " idCampaign " + idCampaign + " textoSMS " + textoSMS);                    
                 }
 
                 // Se cierran los recursos utilizados para la conexión y consulta a la base de datos
--- src/main/java/prueba_sms/bdaquamovil.java
+++ src/main/java/prueba_sms/bdaquamovil.java
@@ -1,52 +1,146 @@
 package prueba_sms;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class bdaquamovil {
 
-	public static void main(String[] args) {
-		
-		//Obtenemos la conexión a la base de datos
+    public static void main(String[] args) {
+        // Obtenemos la conexión a la base de datos
         Connection connection = conexionSQL.getConexion();
-        
-        // Verificamos si la coenxion es éxitosa o no 
+
+        // Verificamos si la conexión es exitosa o no
         if (connection != null) {
             try {
-                // Creamos una declaración con el objeto Statement
-                Statement statement = connection.createStatement();
-
-                // Ejecutamos una consulta donde le pasamos el nombre de la tabla a consultar, el resultado se guarda en el objeto resultSet
-                String query = "SELECT * FROM bdaquamovil.dbo.tblLocales";                                 
-                ResultSet resultSet = statement.executeQuery(query);
-
-     
+            	
+            	//Llamamos a los metodos
+                consultarLocalesPorId(connection, 100);
                 
-                // Recorremos los resultados de la consulta usando el metodo next() para recorrer fila por fila los registros obtenidos en la consulta
-                while (resultSet.next()) {
-                	
-                    //  Utilizamos los métodos getInt() y getString() del ResultSet para obtener los valores de cada columna en el registro actual
-                    int idLocalTblLocales = resultSet.getInt("IDLOCAL");                   
-                    String NombreLocal = resultSet.getString("NombreLocal");
-                    
+                consultarPeriodoFechaPago(connection, 100, 202304);
+                
+                consultarTelefonoCelular(connection, 100);
 
-                    // Mostramos los valores por consola
-                      System.out.println("IDlocal: " + idLocalTblLocales + "  NombreCampaign " + NombreLocal );                    
-                }
-
-                // Se cierran los recursos utilizados para la conexión y consulta a la base de datos
-                resultSet.close();
-                statement.close();
+                // Se cierra la conexión a la base de datos
                 connection.close();
-                
-                //En caso de que ocurra una excepción durante el cierre de los recursos, se captura la excepción SQLException y se imprime la traza de la excepción utilizando el método printStackTrace()
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
+    }// FIN MAIN
+
+                                             // EXTRAER EL ID DEL LOCAL Y SU RAZÓN SOCIAL 
+    private static void consultarLocalesPorId(Connection connection, int idLocal) throws SQLException {
+    	
+        // Ejecutamos una consulta parametrizada. Los signos de interrogación (?) se utilizan como marcadores de posición para los parámetros.
+        String queryRazonSocial = "SELECT * FROM bdaquamovil.dbo.tblLocales WHERE idLocal = ?";
+        PreparedStatement statement = connection.prepareStatement(queryRazonSocial);
+
+        // Asignamos los valores de los parámetros en la consulta
+        statement.setInt(1, idLocal);
+
+        // Se ejecuta la consulta utilizando el método executeQuery() del objeto PreparedStatement y se guarda el resultado en el objeto ResultSet.
+        ResultSet resultSet = statement.executeQuery();
+
+        // Recorremos los resultados de la consulta usando el método next() para recorrer fila por fila los registros obtenidos en la consulta
+        while (resultSet.next()) {
+        	
+            // Utilizamos los métodos getInt() y getString() del ResultSet para obtener los valores de cada columna en el registro actual
+            int idLocales = resultSet.getInt("IDLOCAL");
+            String razonSocial = resultSet.getString("razonSocial");
+
+            // Mostramos los valores por consola
+            System.out.println("Query 1 - IDlocal: " + idLocales + "  razonSocial: " + razonSocial);
+        }
+
+        // Cerramos los recursos utilizados para la consulta
+        resultSet.close();
+        statement.close();
     }
-		
-	
-}
+    
+                                        // EXTRAER ULTIMO PERIODO Y FECHA DE PAGO DEL LOCAL 
+    	private static void consultarPeriodoFechaPago(Connection connection, int idLocal, int idPeriodo ) throws SQLException{
+    		
+    		// Ejecutamos una consulta parametrizada. Los signos de interrogación (?) se utilizan como marcadores de posición para los parámetros.
+            String queryPeriodoFechaPago = "SELECT * FROM bdaquamovil.dbo.tblDctosPeriodo WHERE idLocal = ? AND idPeriodo = ?";
+            PreparedStatement statement = connection.prepareStatement(queryPeriodoFechaPago);
+            
+         // Asignamos los valores de los parámetros en la consulta
+            statement.setInt(1, idLocal);
+            statement.setInt(2, idPeriodo);
+            
+         // Se ejecuta la consulta utilizando el método executeQuery() del objeto PreparedStatement y se guarda el resultado en el objeto ResultSet.
+            ResultSet resultSet = statement.executeQuery();
+            
+         // Recorremos los resultados de la consulta usando el método next() para recorrer fila por fila los registros obtenidos en la consulta
+            while (resultSet.next()) {
+            	
+                // Utilizamos los métodos getInt() y getString() del ResultSet para obtener los valores de cada columna en el registro actual
+                int idLocales = resultSet.getInt("IDLOCAL");
+                int idPeriodos = resultSet.getInt("idPeriodo");
+                String nombrePeriodo = resultSet.getString("nombrePeriodo");
+                String fechaConRecargo = resultSet.getString("fechaConRecargo");
+
+                // Mostramos los valores por consola
+                System.out.println("Query 2 - IDlocal: " + idLocales + "  idPeriodo: " + idPeriodos + " nombrePeriodo: " + nombrePeriodo + " fechaConRecargo: " + fechaConRecargo);
+            }
+
+            // Cerramos los recursos utilizados para la consulta
+            resultSet.close();
+            statement.close();
+    	}
+    	
+    	                                            // EXTRAER NÚMEROS DE TELEFONO CELULAR
+    	private static void consultarTelefonoCelular(Connection connection, int idLocal) throws SQLException{
+    		
+    		// Ejecutamos una consulta parametrizada. Los signos de interrogación (?) se utilizan como marcadores de posición para los parámetros.
+            String queryTelefonoCelular = "SELECT * FROM bdaquamovil.dbo.tblTerceros WHERE idLocal = ? AND ISNUMERIC([telefonoCelular]) = 1 AND LEN([telefonoCelular]) = 10";
+            PreparedStatement statement = connection.prepareStatement(queryTelefonoCelular);
+            
+         // Asignamos los valores de los parámetros en la consulta
+            statement.setInt(1, idLocal);
+            
+         // Se ejecuta la consulta utilizando el método executeQuery() del objeto PreparedStatement y se guarda el resultado en el objeto ResultSet.
+            ResultSet resultSet = statement.executeQuery();
+            
+            // Recorremos los resultados de la consulta usando el método next() para recorrer fila por fila los registros obtenidos en la consulta
+            while (resultSet.next()) {
+            	
+                // Utilizamos los métodos getInt() y getString() del ResultSet para obtener los valores de cada columna en el registro actual
+                int idLocales = resultSet.getInt("IDLOCAL");
+                long telefonoCelular = resultSet.getLong("telefonoCelular");
+                
+
+                // Mostramos los valores por consola
+                System.out.println("Query 3 - IDlocal: " + idLocales + "  telefonoCelular: " + telefonoCelular );
+            }
+
+            // Cerramos los recursos utilizados para la consulta
+            resultSet.close();
+            statement.close();
+    	}
+}// FIN DBAQUAMOVIL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
